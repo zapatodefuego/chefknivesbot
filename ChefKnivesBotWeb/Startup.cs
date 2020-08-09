191139b4a7c7c94717ce8d0d0fcf8b34789cabf8
zapatodefuego <zapatodefuego@gmail.com>
2020-08-09 13:25:30 -0400

    fix dir

--- ChefKnivesBotWeb/Startup.cs
+++ ChefKnivesBotWeb/Startup.cs
@@ -66,7 +66,7 @@ namespace ChefKnivesBotWeb
                 app.UseStaticFiles(new StaticFileOptions()
                 {
                     FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"web")),
-                    RequestPath = new PathString("/web")
+                    RequestPath = new PathString()
                 });
             }
 
