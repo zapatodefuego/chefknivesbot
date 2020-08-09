15cd5655a625916a6fed503b94a1ce4ea98ce4e9
zapatodefuego <zapatodefuego@gmail.com>
2020-08-09 12:04:04 -0400

    remove https for now

--- ChefKnivesBotWeb/Startup.cs
+++ ChefKnivesBotWeb/Startup.cs
@@ -40,7 +40,8 @@ namespace ChefKnivesBotWeb
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
+            
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
