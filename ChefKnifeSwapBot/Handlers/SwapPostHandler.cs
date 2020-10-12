fc2228920aa97840b2a0fa49fec5fac59be21a70
zapatodefuego <zapatodefuego@gmail.com>
2020-10-12 19:30:36 -0400

    develop remove post validation from SwapPostHandler. for now just provide the post history

--- ChefKnifeSwapBot/Handlers/SwapPostHandler.cs
+++ ChefKnifeSwapBot/Handlers/SwapPostHandler.cs
@@ -14,29 +14,15 @@ namespace ChefKnifeSwapBot.Handlers
     public class SwapPostHandler : HandlerBase, IPostHandler
     {
         private const string _postUrlFirstPart = "https://www.reddit.com/r/chefknifeswap/comments/";
-        private const int _numEntrys = 18;
-        private const string _titleEntry = "Selling table. All items mandatory. One table per post. This header must be included";
-        private const string _nameEntry = "Item Name(s)";
-        private const string _descriptionEntry = "Description(s)";
-        private const string _priceEntry = "Asking price(s)";
-        private const string _shippingEntry = "Shipping included in price?";
-        private const string _regionEntry = "Region";
-        private const string _productEntry = "Product page link(s)";
-        private const string _pictureEntry = "Picture album link";
-        private const string _endEntry = "End";
-
-        private static Regex _tableIdentifierRegex = new Regex(";", RegexOptions.Compiled);
 
         private ILogger _logger;
         private readonly SubredditService _service;
-        private readonly FlairV2 _flair;
 
         public SwapPostHandler(ILogger logger, SubredditService service, bool dryRun)
             : base(dryRun)
         {
             _logger = logger;
             _service = service;
-            _flair = service.Subreddit.Flairs.LinkFlairV2.First(f => f.Text.Equals("Selling"));
         }
 
         public bool Process(BaseController controller)
@@ -47,193 +33,35 @@ namespace ChefKnifeSwapBot.Handlers
                 return false;
             }
 
-            if (post.Title.Contains("[Selling]", StringComparison.OrdinalIgnoreCase)|| (_flair != null && _flair.Equals(_flair.Id)))
+            if (_service.SelfCommentDatabase.ContainsAny(nameof(SelfComment.ParentId), post.Id).Result)
             {
-                // Set the flair
-                post.SetFlair(_flair.Text, _flair.Id);
-
-                if (_service.SelfCommentDatabase.ContainsAny(nameof(SelfComment.ParentId), post.Id).Result)
-                {
-                    return false;
-                }
-
-                var body = post.SelfText;
-                var tableMatches = _tableIdentifierRegex.Matches(body);
-                if (tableMatches.Count == 0)
-                {
-                    if (!DryRun)
-                    {
-                        var response = $"It looks like your Selling post is missing the required table or it is formatted incorrectly. [Follow this link to find out more](https://www.reddit.com/r/chefknifeswap/comments/irpqd2/we_will_be_testing_out_new_bot_functions_over_the/)";
-                        var reply = post.Reply(response).Distinguish("yes", true);
-                        //post.Remove();
-
-                        _service.SelfCommentDatabase.Upsert(reply.ToSelfComment(post.Id, RedditThingType.Post));
-                    }
-                    return true;
-                }
-                else if(tableMatches.Count < _numEntrys)
-                {
-                    if (!DryRun)
-                    {
-                        var response = $"It looks like your Selling post does not contain all of the expected entries. [Follow this link to find out more](https://www.reddit.com/r/chefknifeswap/comments/irpqd2/we_will_be_testing_out_new_bot_functions_over_the/)";
-                        var reply = post.Reply(response).Distinguish("yes", true);
-                        //post.Remove();
-
-                        _service.SelfCommentDatabase.Upsert(reply.ToSelfComment(post.Id, RedditThingType.Post));
-                    }
-
-                    return true;
-                }
-                else if (tableMatches.Count > _numEntrys)
-                {
-                    if (!DryRun)
-                    {
-                        var response = $"It looks like your Selling post containes too many entries. [Following this link to find out more](https://www.reddit.com/r/chefknifeswap/comments/irpqd2/we_will_be_testing_out_new_bot_functions_over_the/)";
-                        var reply = post.Reply(response).Distinguish("yes", true);
-                        //post.Remove();
-
-                        _service.SelfCommentDatabase.Upsert(reply.ToSelfComment(post.Id, RedditThingType.Post));
-                    }
-
-                    return true;
-                }
-
-                var formattedRedditTable = new StringBuilder();
-                formattedRedditTable.AppendLine("|Selling table|this was formatted by the bot and may contain errors|");
-                formattedRedditTable.AppendLine(":--|:--|");
-
-                var hasError = false;
-                var errorResponse = new StringBuilder();
-                errorResponse.AppendLine("The following issues were found in your submission:");
-
-                var parts = body.Split(';').Select(s => s.ToLower()).ToArray();
-                if (!GetEntry(parts, _titleEntry, out string titleValue))
-                {
-                    errorResponse.AppendLine("* Title entry was modified, missing, or incorrectly formatted");
-                    hasError = true;
-                }
-
-                if (!GetEntry(parts, _nameEntry, out string nameValue))
-                {
-                    errorResponse.AppendLine("* Item name entry was missing or incorrectly formatted.");
-                    hasError = true;
-                }
-                else
-                {
-                    formattedRedditTable.AppendLine($"|{_nameEntry}|{nameValue.Trim()}|");
-                }
-
-                if (!GetEntry(parts, _descriptionEntry, out string descriptionValue))
-                {
-                    errorResponse.AppendLine("* Description entry was missing or incorrectly formatted.");
-                    hasError = true;
-                }
-                else
-                {
-                    formattedRedditTable.AppendLine($"|{_descriptionEntry}|{descriptionValue.Trim()}|");
-                }
-
-                if (!GetEntry(parts, _priceEntry, out string priceValue))
-                {
-                    errorResponse.AppendLine("* Price entry was missing or incorrectly formatted.");
-                    hasError = true;
-                }
-                else
-                {
-                    formattedRedditTable.AppendLine($"|{_priceEntry}|{priceValue.Trim()}|");
-                }
-
-                if (!GetEntry(parts, _shippingEntry, out string shippingValue))
-                {
-                    errorResponse.AppendLine("* Shipping entry was missing or incorrectly formatted.");
-                    hasError = true;
-                }
-                else
-                {
-                    formattedRedditTable.AppendLine($"|{_shippingEntry}|{shippingValue.Trim()}|");
-                }
-
-                if (!GetEntry(parts, _regionEntry, out string regionValue))
-                {
-                    errorResponse.AppendLine("* Region entry was missing or incorrectly formatted.");
-                    hasError = true;
-                }
-                else
-                {
-                    formattedRedditTable.AppendLine($"|{_regionEntry}|{regionValue.Trim()}|");
-                }
-
-                if (!GetEntry(parts, _productEntry, out string productValue))
-                {
-                    errorResponse.AppendLine("* The product link entry was missing or incorrectly formatted.");
-                    hasError = true;
-                }
-                else
-                {
-                    formattedRedditTable.AppendLine($"|{_productEntry}|{productValue.Trim()}|");
-                }
-
-                if (!GetEntry(parts, _pictureEntry, out string pictureValue))
-                {
-                    errorResponse.AppendLine("* The album link entry was missing or incorrectly formatted.");
-                    hasError = true;
-                }
-                else
-                {
-                    formattedRedditTable.AppendLine($"|{_pictureEntry}|{pictureValue.Trim()}|");
-                }
-
-                if (!DryRun)
-                {
-                    var postHistory = _service.RedditPostDatabase.GetByFilter(nameof(RedditThing.Author), post.Author).Result;
-                    var replyMessage = new StringBuilder();
-                    if (hasError)
-                    {
-                        //post.Remove();
-
-                        errorResponse.AppendLine("Please correct the above issues. [Click this link to find out more.](https://www.reddit.com/r/chefknifeswap/comments/irpqd2/we_will_be_testing_out_new_bot_functions_over_the/)");
-                        errorResponse.AppendLine("\n\n---\n\n");
-                        errorResponse.AppendLine("There were errors but I tried to format the table:\n\n");
-                        replyMessage = errorResponse;
-                    }
-
-                    replyMessage.Append(formattedRedditTable.ToString());
-                    replyMessage.AppendLine("\n\n---\n\n");
-
-                    if (postHistory == null || !postHistory.Any())
-                    {
-                        replyMessage.AppendLine($"u/{post.Author} has not submitted any posts in r/{_service.Subreddit.Name} since I've gained sentience");
-                    }
-                    else
-                    {
-                        replyMessage.AppendLine($"Here are some past posts from u/{post.Author}:");
-                        postHistory.Take(5).ToList()
-                            .ForEach(p => replyMessage.AppendLine($"* [{p.Title}]({_postUrlFirstPart}{p.Id})"));
-                    }
-
-                    var reply = post
-                        .Reply(replyMessage.ToString())
-                        .Distinguish("yes", true);
-
-                    _service.SelfCommentDatabase.Upsert(reply.ToSelfComment(post.Id, RedditThingType.Post));
-                    _service.RedditPostDatabase.Upsert(post.ToPost());
-                }
-
+                return false;
             }
-
-            return false;
-        }
-
-        private bool GetEntry(string[] parts, string name, out string value)
-        {
-            var index = Array.IndexOf(parts, name.ToLower());
-            if (index + 1 < parts.Length)
+            if (!DryRun)
             {
-                value = parts[index+1];
+                var postHistory = _service.RedditPostDatabase.GetByFilter(nameof(RedditThing.Author), post.Author).Result;
+                var replyMessage = new StringBuilder();
+                if (postHistory == null || !postHistory.Any())
+                {
+                    replyMessage.AppendLine($"u/{post.Author} has not submitted any posts in r/{_service.Subreddit.Name} since I've gained sentience");
+                }
+                else
+                {
+                    replyMessage.AppendLine($"Here are some past posts from u/{post.Author}:");
+                    postHistory.Take(5).ToList()
+                        .ForEach(p => replyMessage.AppendLine($"* [{p.Title}]({_postUrlFirstPart}{p.Id})"));
+                }
+
+                var reply = post
+                    .Reply(replyMessage.ToString())
+                    .Distinguish("yes", true);
+
+                _service.SelfCommentDatabase.Upsert(reply.ToSelfComment(post.Id, RedditThingType.Post));
+                _service.RedditPostDatabase.Upsert(post.ToPost());
+
                 return true;
             }
 
-            value = null;
             return false;
         }
     }
